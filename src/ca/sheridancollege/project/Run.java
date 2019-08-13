e2604e45bdd4437bbf26700bdca6222d264db846
parsobry <brydon.parsons@gmail.com>
2019-08-13 15:52:13 -0400

    Input validation added

--- src/ca/sheridancollege/project/Run.java
+++ src/ca/sheridancollege/project/Run.java
@@ -5,52 +5,81 @@ import java.util.*;
 /**
  *
  * @author Brydon Parsons
- * @author Vlad Crihan 
+ * @author Vlad Crihan
  * @author Adit Tandon
  */
-public class Run {
+public class Run{
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         boolean gameOver = false;
         Scanner input = new Scanner(System.in);
 
         System.out.println("Please enter your first name: ");
         String name = input.next();
+        double balance = 0;
 
         System.out.println("Enter your intial balance: ");
-        double balance = input.nextDouble();
+        while(balance <= 0){
+            try{
+                balance = input.nextDouble();
+            }catch(InputMismatchException ex){
+                System.out.println("Please only enter numbers: ");
+                input.next();
+            }
+        }
 
         Deck deck = new Deck();
         Game game = new Game(new Player(name), new Dealer(), deck);
 
-        while (balance < 10) {
-            if (balance < 10) {
+        while(balance < 10){
+            if(balance < 10){
                 System.out.println("Intial balance should be greater than"
                         + "$10");
-                balance = input.nextDouble();
+                try{
+                    balance = input.nextDouble();
+                }catch(InputMismatchException ex){
+                    System.out.println("Please only enter numbers: ");
+                    input.next();
+                }
             }
         }
         game.getP().getBalance().setTotalBalance(balance);
 
         System.out.println("Welcome to BlackJack!");
-        while (gameOver == false) {// This is the main loop of the game
-            if (deck.getDeckSize() < 20) {
+        while(gameOver == false){// This is the main loop of the game
+            double bet = 0;
+            if(deck.getDeckSize() < 20){//Resets deck when it gets low
                 deck = new Deck();
             }
             System.out.println("Please enter your bet(Minimum $10): ");
-            double bet = input.nextDouble();
-            while (bet > game.getP().getBalance().getTotalBalance()) {
+            try{
+                bet = input.nextDouble();
+            }catch(InputMismatchException ex){
+                System.out.println("Please only enter numbers: ");
+                input.next();
+            }
+            while(bet > game.getP().getBalance().getTotalBalance()){
                 System.out.println("Your bet is more than your balance. "
                         + "Enter a new bet");
-                bet = input.nextDouble();
+                try{
+                    bet = input.nextDouble();
+                }catch(InputMismatchException ex){
+                    System.out.println("Please only enter numbers: ");
+                    input.next();
+                }
             }
-            while (bet < 10) {
-                System.out.println("Minimum bet should be $10! \nEnter again");
-                bet = input.nextDouble();
+            while(bet < 10){
+                try{
+                    System.out.println("Minimum bet should be $10! \nEnter again");
+                    bet = input.nextDouble();
+                }catch(InputMismatchException ex){
+                    System.out.println("Please only enter numbers: ");
+                    input.next();
+                }
             }
             //player get's 2 cards
-            for (int i = 0; i < 2; i++) {
+            for(int i = 0; i < 2; i++){
                 game.getP().addCard(game.getDeck().getTopCard());
             }
             //Dealer get's a card
@@ -66,19 +95,25 @@ public class Run {
             //start of the player's turn
             System.out.println(game.getP().getPlayerID() + "'s turn: ");
             System.out.println("Would you like to HIT or STAND?");
-            String action = input.next().toUpperCase();
+            String action = "";
+
+            action = input.next().toUpperCase();
+            while(!action.equals("HIT") && !action.equals("STAND")){
+                System.out.println("Please choose Hit or Stand");
+                action = input.next().toUpperCase();
+            }
 
             boolean playerBusted = false;
-            while (action.equals("HIT")) {
+            while(action.equals("HIT")){
                 game.getP().addCard(game.getDeck().getTopCard());
-                if (game.getP().cardTotal() > 21) {
-                    System.out.println("\n"+game.getP().printHand(name));
+                if(game.getP().cardTotal() > 21){
+                    System.out.println("\n" + game.getP().printHand(name));
                     System.out.println("Dealer won! Player busted");
                     playerLose(game, bet);
                     playerBusted = true;
                     break;
                 }
-                System.out.println("\n"+game.getP().printHand(name));
+                System.out.println("\n" + game.getP().printHand(name));
                 System.out.println("Would you like to HIT or STAND?");
                 action = input.next().toUpperCase();
 
@@ -86,14 +121,14 @@ public class Run {
 
             //dealer's turn
             boolean dealerBusted = false;
-            if (!playerBusted) {
+            if(!playerBusted){
                 game.getD().addCard(game.getDeck().getTopCard());
-                System.out.println("\n"+game.getD().printHand("Dealer"));
-                while (game.getD().cardTotal() < 16) {
+                System.out.println("\n" + game.getD().printHand("Dealer"));
+                while(game.getD().cardTotal() < 16){
                     System.out.println("*** Dealer Draws ***");
                     game.getD().addCard(game.getDeck().getTopCard());
                     System.out.println(game.getD().printHand("Dealer"));
-                    if (game.getD().cardTotal() > 21) {
+                    if(game.getD().cardTotal() > 21){
                         System.out.println(game.getD().printHand("Dealer"));
                         playerWin(game, bet);
                         dealerBusted = true;
@@ -103,11 +138,10 @@ public class Run {
             }
 
             //check for win
-            if (!dealerBusted && !playerBusted) {
-                if (game.getP().cardTotal() > game.getD().cardTotal()) {
+            if(!dealerBusted && !playerBusted){
+                if(game.getP().cardTotal() > game.getD().cardTotal()){
                     playerWin(game, bet);
-
-                } else {
+                }else{
                     playerLose(game, bet);
                 }
             }
@@ -116,33 +150,33 @@ public class Run {
             System.out.println(game.getP().printBalance());
 
             //ask player if you want to play again
-            if (game.getP().getBalance().getTotalBalance() > 10) {
+            if(game.getP().getBalance().getTotalBalance() > 10){
                 System.out.println("Do you want to play again?");
-                if (input.next().toUpperCase().equals("NO")) {
+                if(input.next().toUpperCase().equals("NO")){
                     gameOver = true;
-                } else {
+                }else{
                     System.out.println("\n");
-                    for (int i = 0; i < 100; i++) {
+                    for(int i = 0; i < 100; i++){
                         System.out.print("-");
                     }
                     System.out.println("\n\n");
                 }
             }
-            
+
             //reset hands
             game.getP().getHand().clear();
             game.getD().getHand().clear();
         }
     }
 
-    public static void playerWin(Game game, double bet) {
+    public static void playerWin(Game game, double bet){
         game.getP().getBalance().addBalance(
                 bet * game.getWIN_RATIO());
         System.out.println(game.getP().getPlayerID() + " won $"
                 + bet * game.getWIN_RATIO());
     }
 
-    public static void playerLose(Game game, double bet) {
+    public static void playerLose(Game game, double bet){
         game.getP().getBalance().loseBalance(bet);
         System.out.println("Dealer wins\n" + game.getP().getPlayerID()
                 + " lost $" + bet);
