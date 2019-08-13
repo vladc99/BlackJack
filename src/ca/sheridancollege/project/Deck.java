e29029c72c7b0bb8ed1928fa232df0e85958f34d
Vlad <Vlad@10.16.0.144>
2019-08-13 17:58:16 -0400

    JUnit Testing

--- src/ca/sheridancollege/project/Deck.java
+++ src/ca/sheridancollege/project/Deck.java
@@ -15,7 +15,7 @@ public class Deck{
 
         //Code to build a deck of 52 cards
         for(int i = 0; i < 4; i++){// 4 suits
-            for(int j = 2; j < 14; j++){// Cards 2-10
+            for(int j = 1; j < 14; j++){// Cards 2-10
                 deck.add(new Card(j, i));
             }
         }
